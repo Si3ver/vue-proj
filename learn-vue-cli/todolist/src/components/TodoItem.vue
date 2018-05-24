5a74f7844722da2bd2fde044596f9c29566b9e1b
Si3ver <zhou_weilin@outlook.com>
2018-05-24 23:08:19 +0800

    publish

---
+++ learn-vue-cli/todolist/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+    props: ['content', 'index'],
+    methods: {
+        handleDelete () {
+            this.$emit('delete', this.index)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.item {
+    color: red;
+}
+li {
+  list-style-type: decimal;
+}
+li.complete {
+  text-decoration: line-through;
+}
+</style>
